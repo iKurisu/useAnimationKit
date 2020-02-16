a5740839a2d321826fc18743a13969f79f7f0e11
iKurisu <chris.epic01@gmail.com>
2020-02-16 15:14:25 +0100

    add useDidUpdateEffect

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export { default as useFrame } from "./useFrame";
 export { default as useTransition } from "./useTransition";
 export { default as useCustomScroll } from "./useCustomScroll";
 export { default as useParallax } from "./useParallax";
+export { default as useDidUpdateEffect } from "./useDidUpdateEffect";
+export { default as useIntersection } from "./useIntersection";
 export { createScrollContext } from "./utils/context";
