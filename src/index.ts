4c611ebedfec78f42bcf42d276e18b7db95633e1
iKurisu <chris.epic01@gmail.com>
2020-02-16 11:01:51 +0100

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import useAnimationFrame from "./useAnimationFrame";
-import useFrame from "./useFrame";
-import useTransition from "./useTransition";
-import useCustomScroll from "./useCustomScroll";
-import useParallax from "./useParallax";
-import { createScrollContext } from "./utils/context";
-
-export default {
-  useAnimationFrame,
-  useFrame,
-  useTransition,
-  useCustomScroll,
-  useParallax,
-  createScrollContext,
-};
+export { default as useAnimationFrame } from "./useAnimationFrame";
+export { default as useFrame } from "./useFrame";
+export { default as useTransition } from "./useTransition";
+export { default as useCustomScroll } from "./useCustomScroll";
+export { default as useParallax } from "./useParallax";
+export { createScrollContext } from "./utils/context";
