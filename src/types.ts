2a7469ecd25ed8e9e2c8bc7c6bef066b820554d4
iKurisu <chris.epic01@gmail.com>
2020-02-16 10:56:43 +0100

    extract types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,15 @@
 export type CubicBezier = [number, number, number, number];
+
+export interface ManualScrollConfig {
+  to: number;
+  duration: number;
+  timing?: CubicBezier;
+}
+
+/** The function that will get called when the user scrolls. */
+export type Listener = (scroll: number, max: number) => void;
+
+export type Subscriber = (listener: Listener) => void;
+export type Unsubscriber = (listener: Listener) => void;
+
+export type ManualScroller = (config: ManualScrollConfig) => void;
