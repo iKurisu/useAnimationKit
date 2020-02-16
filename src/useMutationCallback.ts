2fc7658194b5bfa39dd80006c7498c194711671b
iKurisu <chris.epic01@gmail.com>
2020-02-16 10:55:38 +0100

    add options

--- src/useMutationCallback.ts
+++ src/useMutationCallback.ts
@@ -1,5 +1,11 @@
 import { useRef, useEffect, RefObject } from "react";
 
+const config = {
+  attributes: true,
+  childList: true,
+  characterData: true,
+};
+
 /**
  * Creates a mutation observer on a given node that will call the callback
  * each time the node changes.
@@ -7,6 +13,7 @@ import { useRef, useEffect, RefObject } from "react";
 const useMutationCallback = (
   node: RefObject<HTMLElement>,
   callback: MutationCallback,
+  options: MutationObserverInit = config,
 ): void => {
   const observer = useRef<MutationObserver | null>(null);
 
@@ -20,7 +27,7 @@ const useMutationCallback = (
 
   useEffect((): (() => void) => {
     const observer = getObserver();
-    if (node.current) observer.observe(node.current);
+    if (node.current) observer.observe(node.current, options);
     return (): void => observer.disconnect();
   });
 };
